efedbae1d85141d620a11da4518236034f511579
OiC-b8024-Inoue <43872843+Typhoon0x73@users.noreply.github.com>
2022-08-09 23:29:52 +0900

    Update Main.cpp

--- siv3d_rule_shader/Main.cpp
+++ siv3d_rule_shader/Main.cpp
@@ -1,6 +1,45 @@
 ﻿# include <Siv3D.hpp> // OpenSiv3D v0.6.4
 
+//#define CUSTOM_SELECT_TEXTURE
 SIV3D_SET(EngineOption::Renderer::OpenGL); // Windows で Direct3D の代わりに OpenGL を使用
+
+const Array<String> custom_texture_paths {
+	U"Resources/rule800/11.png",
+	U"Resources/rule800/39.png",
+	U"Resources/rule800/40.png",
+	U"Resources/rule800/54.png",
+	U"Resources/rule800/97.png",
+	U"Resources/rule800/115.png",
+	U"Resources/rule800/150.png",
+	U"Resources/rule800/224.jpg",
+	U"Resources/rule800/226.jpg",
+};
+
+#ifdef CUSTOM_SELECT_TEXTURE
+const uint32_t texture_count = custom_texture_paths.size();
+#else
+const uint32_t texture_count = 233;
+#endif//CUSTOM_SELECT_TEXTURE
+
+void LoadTextures(Array<Texture>* v1, ListBoxState* v2)
+{
+	assert(v1);
+	assert(v2);
+	v1->reserve(texture_count);
+#ifdef CUSTOM_SELECT_TEXTURE
+	for (const auto& path : custom_texture_paths)
+	{
+		v2->items.push_back(FileSystem::FileName(path));
+		v1->emplace_back(Texture{ path });
+	}
+#else
+	for (int i = 0; i < texture_count; i++)
+	{
+		v2->items.push_back(U"{0}.{1}"_fmt(i + 1, (i > 175 ? U"jpg" : U"png")));
+		v1->emplace_back(Texture{ U"Resources/rule800/{0}"_fmt(v2->items[i]) });
+	}
+#endif // CUSTOM_SELECT_TEXTURE
+}
 
 // シェーダー側と合わせたバッファ構造体
 struct RuleFade
@@ -30,12 +69,7 @@ void Main()
 	// 画像読み込みと切り替え用リストアイテム作成
 	Array<Texture> masks;
 	ListBoxState lbs;
-	masks.reserve(233);
-	for (int i = 0; i < 233; i++)
-	{
-		lbs.items.push_back(U"{0}.{1}"_fmt(i + 1, (i > 175 ? U"jpg" : U"png")));
-		masks.emplace_back(Texture{ U"Resources/rule800/{0}"_fmt(lbs.items[i])});
-	}
+	LoadTextures(&masks, &lbs);
 	int select = 0;
 	lbs.selectedItemIndex = select;
 
