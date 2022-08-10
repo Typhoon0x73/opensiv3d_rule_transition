4b1261adae9b4c92a1aaff05eb0b44ba6be87494
OiC-b8024-Inoue <43872843+Typhoon0x73@users.noreply.github.com>
2022-08-10 20:17:45 +0900

    Update rule_fade.hlsl

--- siv3d_rule_shader/App/Resources/rule_fade.hlsl
+++ siv3d_rule_shader/App/Resources/rule_fade.hlsl
@@ -52,6 +52,6 @@ float4 PS(s3d::PSInput input) : SV_TARGET
     float alpha = smoothstep(minValue, maxValue, maskValue);
     
     // 割り当てたテクスチャを使う
-    float4 col = g_texture0.Sample(g_sampler0, input.uv);
-    return ((float4(color.rgb, alpha) * input.color) + g_colorAdd);
+    float4 col = g_texture0.Sample(g_sampler0, input.uv) * input.color + g_colorAdd;
+    return float4(col.rgb, alpha);
 }
